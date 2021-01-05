0329b0e42936123b64acb082f926f3b60dd7e5bb
Leandro Agustín Szikora <lszikora@dblandit.com>
2021-01-05 10:38:16 -0300

    Demo 2

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     res.status(404).json({
         code: 12,
         message: {
-            description: "Recurso no encontrado >:("
+            description: "Recurso no encontrado >:( !"
         }
     })
 });
